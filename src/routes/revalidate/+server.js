01f5f797027f3d4f2b9bf34da5da9b5c5c4eb287
Gabriele Cicconetti <gacicconetti@gmail.com>
2024-07-19 10:57:35 +0200

    -

--- src/routes/revalidate/+server.js
+++ src/routes/revalidate/+server.js
@@ -1,13 +1,13 @@
 import { json } from '@sveltejs/kit';
 
-export async function POST({ request, cookies, fetch }) {
+export async function POST({ request, cookies, fetch, ...event }) {
 	const data = await request.json();
-	console.log(data);
-	const res = await fetch('/', {
-		headers: {
-			'x-prerender-revalidate': 'vOINkES15fLSseVSSERuvOINkES15fLSseVSSERuvOINkES15fLSseVSSERuvOINkES15fLSseVSSERuvOINkES15fLSseVSSERu'
-		}
-	});
+	console.log(event.getClientAddress());
+	// const res = await fetch('/', {
+	// 	headers: {
+	// 		'x-prerender-revalidate': 'vOINkES15fLSseVSSERuvOINkES15fLSseVSSERuvOINkES15fLSseVSSERuvOINkES15fLSseVSSERuvOINkES15fLSseVSSERu'
+	// 	}
+	// });
 
 	return json({}, { status: 200 });
 }
